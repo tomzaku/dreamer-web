2f8588d310b83e5a66335389375dd74e7329f084
Tom Le <tom.levt@shopee.com>
2022-09-21 00:20:35 +0800

    feat: start using motion

--- packages/moon-ui/toggle/src/index.tsx
+++ packages/moon-ui/toggle/src/index.tsx
@@ -1,5 +1,24 @@
-import 'react-toggle/style.css';
+import cx from 'classnames'
+import { motion } from 'framer-motion';
 
-import Toggle from 'react-toggle';
+import styles from './index.module.scss'
 
-export default Toggle;
+const spring = {
+  type: 'spring',
+  stiffness: 700,
+  damping: 30,
+};
+
+export default function Toggle({
+  checked,
+  onChange,
+}: {
+  checked: boolean;
+  onChange: () => void;
+}) {
+  return (
+    <div className={cx(styles.container, checked && styles.checkedContainer)} onClick={onChange}>
+      <motion.div className={styles.handle} layout transition={spring} />
+    </div>
+  );
+}
